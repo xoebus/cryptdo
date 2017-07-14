35975e9704a6330ed6edc630dbb8639de7673ca5
Christopher Brown <xoebus@xoeb.us>
2017-07-14 14:47:41 -0700

    godoc and fix linting errors in passphrase.go

--- passphrase.go
+++ passphrase.go
@@ -9,13 +9,20 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+// ErrEmptyPassphase is returned when the user does not type a passphrase
+// before pressing return.
 var ErrEmptyPassphase = errors.New("cryptdo: passphrase must not be empty")
 
+// ReadPassphrase presents a prompt to the user before allowing them to type a
+// passphrase without echoing the characters they type back to the terminal. It
+// returns ErrEmptyPassphase if the user does not enter a passphrase.
 func ReadPassphrase(prompt string) (string, error) {
-	fmt.Fprintf(os.Stderr, "%s: ", prompt)
+	if _, err := fmt.Fprintf(os.Stderr, "%s: ", prompt); err != nil {
+		return "", err
+	}
 	defer fmt.Println()
 
-	input, err := terminal.ReadPassword(int(syscall.Stdin))
+	input, err := terminal.ReadPassword(syscall.Stdin)
 	if err != nil {
 		return "", err
 	}
